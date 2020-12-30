83edef579a8d2094fdc30574d4214ef2491232b8
singhshreshtha <46536071+singhshreshtha@users.noreply.github.com>
2020-12-30 13:14:22 +0530

    Add files via upload
    Uploaded mysql export file and answers of queries in assesment3 file.

---
+++ adjacencytreeassesment3.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2020 at 08:40 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adjacencytreeassesment3`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `calculate_node_level` ()  BEGIN 
+WITH RECURSIVE e_products_path (id, prod_name, lvl) AS
+(
+  SELECT id, prod_name, 0 lvl
+    FROM e_products
+    WHERE root_id IS NULL
+  UNION ALL
+  SELECT ep1.id, ep1.prod_name,ep2.lvl + 1
+    FROM e_products_path AS ep2 JOIN e_products AS ep1
+      ON ep2.id = ep1.root_id
+)
+SELECT * FROM e_products_path
+ORDER BY lvl;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `immediate_children` (IN `rootID` INT)  BEGIN 
+SELECT id,prod_name FROM e_products WHERE root_id = rootID;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `e_products`
+--
+
+CREATE TABLE `e_products` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `prod_name` varchar(50) NOT NULL,
+  `root_id` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `e_products`
+--
+
+INSERT INTO `e_products` (`id`, `prod_name`, `root_id`) VALUES
+(1, 'Electronics', NULL),
+(2, 'Laptops & PC', 1),
+(3, 'Laptops', 2),
+(4, 'PC', 2),
+(5, 'Cameras & photo', 1),
+(6, 'Camera', 5),
+(7, 'Phones & Accessories', 1),
+(8, 'Smartphones', 7),
+(9, 'Android', 8),
+(10, 'ios', 8),
+(11, 'Other Smartphones', 8),
+(12, 'Batteries', 7),
+(13, 'Headsets', 7),
+(14, 'Screen Protectors', 7);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `e_products`
+--
+ALTER TABLE `e_products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `root_id` (`root_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `e_products`
+--
+ALTER TABLE `e_products`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `e_products`
+--
+ALTER TABLE `e_products`
+  ADD CONSTRAINT `e_products_ibfk_1` FOREIGN KEY (`root_id`) REFERENCES `e_products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
